224c82975d0d783aed34d069e47084012877217c
Caique Zaneti Kirilo <caiquez.kirilo@hotmail.com>
2023-09-12 21:16:37 -0300

    Fim da acao

--- Program.cs
+++ Program.cs
@@ -1,7 +1,32 @@
 ﻿using System.Runtime.InteropServices.Marshalling;
 using Acao_12_09.Classes;
 
-Console.WriteLine("Hello, World!");
+string opcao = "";
+do{
+Console.WriteLine(@"
 
+1 - Cadastrar PJ
+2 - Sair
+
+");
+
+opcao = Console.ReadLine();
+
+if (opcao == "1"){
+PessoaJuridica pj = new PessoaJuridica();
+
+Console.WriteLine("Digite o nome da Empresa");
+pj.Nome = Console.ReadLine();
+
+Console.WriteLine("Digite o rendimento da Empresa");
+pj.Rendimento = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o CNPJ da Empresa");
+pj.CNPJ = Console.ReadLine();
+
+pj.Inserir(pj);
+
+}
+}while (opcao != "2");
 
 
